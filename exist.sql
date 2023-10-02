bb074e885cb3a0baf98ae07042c90e9cf75e205d
Elena Ipatova <99578851+habbena@users.noreply.github.com>
2023-10-02 17:47:04 +0300

    Update exist.sql

--- exist.sql
+++ exist.sql
@@ -1,1 +1,25 @@
+-- Выбрать клиентов, которые делали более 2 заказов
+
+SELECT
+    customer_id,
+    first_name,
+    last_name
+FROM
+    sales.customers c
+WHERE
+    EXISTS (
+        SELECT
+            COUNT (*)
+        FROM
+            sales.orders o
+        WHERE
+            customer_id = c.customer_id
+        GROUP BY
+            customer_id
+        HAVING
+            COUNT (*) > 2
+    )
+ORDER BY
+    first_name,
+    last_name;
 
